2429a0293e81303b0c3127560d685c6da0a3818d
dquach217 <dq6180@rit.edu>
2019-10-06 00:06:15 -0400

    fix: clean the code

--- minc/push.py
+++ minc/push.py
@@ -22,41 +22,19 @@ def push_notification(registration_ids, title, body, icon, message):
 
 def logger(res):
     # List of requests.Response objects from GCM Server.
-    logging.basicConfig(level=logging.DEBUG, filename="requests", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.responses)
-
     # List of messages sent.
-    logging.basicConfig(level=logging.DEBUG, filename="messages", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.messages)
-
     # List of registration ids sent.
-    logging.basicConfig(level=logging.DEBUG, filename="registration_ids", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.registration_ids)
-
     # List of server response data from GCM.
-    logging.basicConfig(level=logging.DEBUG, filename="response_data", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.data)
-
     # List of successful registration ids.
-    logging.basicConfig(level=logging.DEBUG, filename="successes", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.successes)
-
     # List of failed registration ids.
-    logging.basicConfig(level=logging.DEBUG, filename="\failures", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.failures)
-
     # List of exceptions.
-    logging.basicConfig(level=logging.DEBUG, filename="errors", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.errors)
-
     # List of canonical ids (registration ids that have changed).
-    logging.basicConfig(level=logging.DEBUG, filename="canonical_ids", filemode="a+",
-                        format="%(asctime)-15s %(levelname)-8s %(message)s")
-    logging.info(res.canonical_ids)
+    response_body = ["requests", "messages", "registration_ids", "data",
+                     "successes", "failures", "errors", "canonical_ids"]
+    for i in response_body:
+        log_name = i + ".log"
+        logfile = os.path.join(os.pardir, log_name)
+        logging.basicConfig(level=logging.DEBUG, filename=logfile, filemode="a+",
+                            format="%(asctime)-15s %(levelname)-8s %(message)s")
+
+        logging.info(getattr(res, i))
